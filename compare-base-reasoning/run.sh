7392881a6f3c3edda0adc492d50fcdba664507fa
Ivan Arcuschin <iarcuschin@gmail.com>
2025-06-11 11:43:38 +0000

    Use more tokens for comparing base reasoning exp

--- compare-base-reasoning/run.sh
+++ compare-base-reasoning/run.sh
@@ -1,12 +1,12 @@
 # Distill models
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model deepseek/deepseek-r1-distill-llama-8b # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model deepseek/deepseek-r1-distill-llama-70b # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model deepseek/deepseek-r1-distill-qwen-1.5b # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model deepseek/deepseek-r1-distill-qwen-14b # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model deepseek/deepseek-r1-distill-qwen-32b # Available in OpenRouter
-# Instruct models
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model meta-llama/llama-3.1-8b-instruct # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model meta-llama/llama-3.3-70b-instruct # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model Qwen/Qwen2.5-Math-1.5B # Not available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model Qwen/Qwen2.5-14B-Instruct # Not available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 500 --model qwen/qwen2.5-32b-instruct # Available in OpenRouter
+python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model deepseek/deepseek-r1-distill-llama-8b # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model deepseek/deepseek-r1-distill-llama-70b # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model deepseek/deepseek-r1-distill-qwen-1.5b # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model deepseek/deepseek-r1-distill-qwen-14b # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model deepseek/deepseek-r1-distill-qwen-32b # Available in OpenRouter
+# # Instruct models
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model meta-llama/llama-3.1-8b-instruct # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model meta-llama/llama-3.3-70b-instruct # Available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model Qwen/Qwen2.5-Math-1.5B # Not available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model Qwen/Qwen2.5-14B-Instruct # Not available in OpenRouter
+# python compare_reasoning.py --n_examples 100 --max_tokens 2000 --model qwen/qwen2.5-32b-instruct # Available in OpenRouter
