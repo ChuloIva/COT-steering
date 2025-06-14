1e8b313dc1a33c4caa0f78c72b653cd79c9b4a87
Ivan Arcuschin <iarcuschin@gmail.com>
2025-06-14 14:41:36 +0000

    Minor change

--- compare-base-reasoning/run.sh
+++ compare-base-reasoning/run.sh
@@ -7,8 +7,8 @@ python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_js
 # Instruct models
 python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --skip_viz --model meta-llama/llama-3.1-8b-instruct # Available in OpenRouter
 python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --skip_viz --model meta-llama/llama-3.3-70b-instruct # Available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 2000 --skip_viz --model Qwen/Qwen2.5-1.5B-Instruct # Not available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 2000 --skip_viz --model Qwen/Qwen2.5-14B-Instruct # Not available in OpenRouter
-python compare_reasoning.py --n_examples 100 --max_tokens 2000 --skip_viz --model Qwen/Qwen2.5-32B-Instruct # Not available in OpenRouter
+python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --skip_viz --model Qwen/Qwen2.5-1.5B-Instruct # Not available in OpenRouter
+python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --skip_viz --model Qwen/Qwen2.5-14B-Instruct # Not available in OpenRouter
+python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --skip_viz --model Qwen/Qwen2.5-32B-Instruct # Not available in OpenRouter
 # Make visualization, any model will do
 python compare_reasoning.py --n_examples 100 --max_tokens 2000 --compute_from_json --model Qwen/Qwen2.5-32B-Instruct
